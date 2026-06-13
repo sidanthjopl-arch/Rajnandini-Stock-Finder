328cea45e32e8678caf32f24899d4885e7e82ff3
sidanthjopl-arch <sidanthjopl@gmail.com>
2026-06-13 22:13:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const stockSheetUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQxQ6tho5vRnTD1w94B0xAu3wzj8MTL0iWqyppyxEHsPy_u1lVeIXi5K_Y0xMNu1dL-kOcP9JIFx1pR/pub?output=csv';
-const imageSheetUrl='YOUR_IMAGE_CSV_URL';
+const imageSheetUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQxQ6tho5vRnTD1w94B0xAu3wzj8MTL0iWqyppyxEHsPy_u1lVeIXi5K_Y0xMNu1dL-kOcP9JIFx1pR/pub?output=csv';
 document.getElementById('themeBtn').onclick=()=>document.body.classList.toggle('dark');
