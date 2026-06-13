c5aa054bc7e4e1b2215a1b25a331eb011e5b97f1
sidanthjopl-arch <sidanthjopl@gmail.com>
2026-06-13 22:15:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const stockSheetUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQxQ6tho5vRnTD1w94B0xAu3wzj8MTL0iWqyppyxEHsPy_u1lVeIXi5K_Y0xMNu1dL-kOcP9JIFx1pR/pub?output=csv';
-const imageSheetUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQxQ6tho5vRnTD1w94B0xAu3wzj8MTL0iWqyppyxEHsPy_u1lVeIXi5K_Y0xMNu1dL-kOcP9JIFx1pR/pub?output=csv';
+const imageSheetUrl='https://docs.google.com/spreadsheets/d/e/2PACX-1vQMYiDx_noTYEQffm5508rXIcFyuHSecEKRlpVXGYIhmYWhWZgSRXsHLoOQTeuy7PG8D-7RF8Op-0Uq/pub?output=csv';
 document.getElementById('themeBtn').onclick=()=>document.body.classList.toggle('dark');
